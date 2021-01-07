2d58b4b11b692341f431ece50b18ac5a06ade5e3
GVRS SAI GANESH <sai102789@gmail.com>
2021-01-08 02:04:30 +0530

    Starter Updated

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,8 @@
+let js = "amazing"; //simple js example
+      if (js === "amazing") {
+        alert("javascript is FUN!");
+      } else {
+        alert("javascript is BAD!");
+      }
+      40 + 8 + 23 + 10; // only adding here won't give the result in console. We need to to explicitly mention in the console.log() function to get its value displayed in console
+      console.log(40 + 8 + 23 + 10);
